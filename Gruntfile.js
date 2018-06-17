68eeb03268b79df448bb55e27ace220c60f67e2e
hs <>
2018-06-17 13:42:52 +0200

    created docs

--- Gruntfile.js
+++
@@ -1,5 +0,0 @@
-/*global module:false*/
-module.exports = function(grunt) {
-    const sgc  = require('../../../sharedGruntConfig')(grunt, __dirname, ['hsLayout'], 'lib');
-	grunt.initConfig(sgc); 
-};
