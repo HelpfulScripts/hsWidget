74267708db4e26781ac612800c1c9b136292957b
hs <>
2018-07-01 14:04:50 +0200

    added sliders and labels

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@
 export { Menu }         from './Menu'; 
 export { SelectorDesc } from './Selector'; 
 export { Button }       from './Button'; 
+export { Label }        from './Label'; 
+export { Slider }       from './Slider'; 
 export { RadioButton }  from './RadioButton'; 
 export { ToggleButton } from './ToggleButton'; 
-export { ToolbarButton,
-         ButtonSymbols
-        }               from './ToolbarButton';
-export { AddButton, 
-         RemoveButton } from './AddRemove'; 
+export { ToolbarButton} from './ToolbarButton'; 
+export { ButtonSymbols} from './ToolbarButton';          
+export { AddButton }    from './AddRemove'; 
+export { RemoveButton } from './AddRemove'; 
 export { Collapsible }  from './Collapsible'; 
 export { Modal }        from './Modal'; 
 export { TypeAhead }    from './TypeAhead'; 
