0491a85080152c2f13eae50ae2f11156c4262119
hs <>
2018-07-04 21:49:54 +0200

    fixed grunt path

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*global module:false*/
 module.exports = function(grunt) {
-    const sgc  = require('../../../sharedGruntConfig')(grunt, __dirname, ['hsLayout'], 'lib');
+    const sgc  = require('./sharedGruntConfig')(grunt, __dirname, ['hsLayout'], 'lib');
 	grunt.initConfig(sgc); 
 };
