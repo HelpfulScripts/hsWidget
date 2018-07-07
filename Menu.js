b138a868fffce4b35423b945fe457aed0435b757
hs <>
2018-07-07 12:16:34 +0200

    removed .js files from root

--- Menu.js
+++
@@ -1,6 +0,0 @@
-import { RadioButton } from './RadioButton';
-export class Menu extends RadioButton {
-    view(node) { return this.viewGroup('.hs-menu', node); }
-}
-;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTWVudS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9NZW51LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQXdEQSxPQUFPLEVBQUUsV0FBVyxFQUFFLE1BQU8sZUFBZSxDQUFDO0FBTTdDLE1BQU0sV0FBWSxTQUFRLFdBQVc7SUFDakMsSUFBSSxDQUFDLElBQVcsSUFBVyxPQUFPLElBQUksQ0FBQyxTQUFTLENBQUMsVUFBVSxFQUFFLElBQUksQ0FBQyxDQUFDLENBQUMsQ0FBQztDQUN4RTtBQUFBLENBQUMifQ==
