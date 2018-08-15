564b17d6836f2d6af49216e7f7a81f95bc776b90
hs <>
2018-08-15 14:34:35 -0700

    moved state logic inside Modal

--- src/Modal.jest.ts
+++ src/Modal.jest.ts
@@ -1,5 +1,44 @@
-import * as widgets from './';
+import * as hswidget from './';
+import { m }    from 'hslayout';
+// import { Log }  from 'hsnode'; const log = Log('Button.jest');
 
-test('dummy test', ()=>{
-    expect(widgets).toBeDefined();
+window = Object.assign(require('mithril/test-utils/domMock.js')(), require('mithril/test-utils/pushStateMock')());
+const mq = require('mithril-query');
+
+let dismissals = 0;
+let trigger;
+
+describe('ModalDialog', () => {
+    const out = mq(m('#parent', {
+            onclick: () => trigger()
+        }, 
+        m(hswidget.Modal, {
+            width:  '300px',
+            height: '200px',
+            dismiss: () => dismissals++,
+            setTrigger: (t) => trigger = t,
+            content: m('', `click on border or on the x to release ${dismissals}`)
+        })
+    ));
+    it ('should have DOM structure', () => {
+        out.should.not.have('#parent>.hs-modal-frame');
+    });
+    it ('should open on trigger', () => {
+        expect(dismissals).toBe(0);
+        out.click('#parent');
+        out.should.have('#parent>.hs-modal-frame');
+        out.should.have('.hs-modal-frame>.hs-modal-background');
+        out.should.have('.hs-modal-frame>.hs-modal-foreground[style="width:300px; height:200px;"]');
+        out.should.have('.hs-modal-frame>.hs-modal-foreground>.hs-corner-button');
+        out.should.have('.hs-modal-frame>.hs-modal-foreground>.hs-corner-button[onclick]');
+        out.should.contain('click on border or on the x to release');    
+    });
+    it('should close on clicking the x', () => {
+        expect(dismissals).toBe(0);
+        out.click('.hs-modal-frame>.hs-modal-foreground>.hs-corner-button', null, false);
+        // out.log('#parent');
+        expect(dismissals).toBe(1);
+        out.should.not.have('#parent>.hs-modal-frame');
+        out.should.have('#parent>span');
+    });
 });
--- src/Modal.ts
+++ src/Modal.ts
@@ -9,20 +9,24 @@
  * - `width?:  string` the `px` or `%` of the window width to use, or 'auto' if omitted.
  * - `height?: string` the `px` or `%` of the window height to use, or 'auto' if omitted.
  * - `content: Vnode` the mithril node to show as content of the modal
- * - `dismiss: ()=>void` a function that is called when the modal box is dismissed
+ * - `dismiss: () => void` an optional function that is called when the modal box is dismissed.
+ * - `setTrigger: (trigger:()=>void)=>void` required; a function that that receives a trigger function 
+ *      to be called in order to trigger the modal box. See below for an implementation example.
  * 
  * ### Example
  * <example>
  * <file name='script.js'>
- * let showModal = false;
+ * let dismissals = 0;
+ * let trigger;
  * m.mount(root, {view: () => m('.hs-white', [
- *      m('h4', {onclick:() => showModal = true }, 'click me'),
- *      showModal? m(hswidget.Modal, { 
+ *      m('h4', {onclick:() => trigger()}, `click me (dismissed ${dismissals} times)`),
+ *      m(hswidget.Modal, { 
+ *          setTrigger: (t) => trigger = t,
  *          width:  '300px',
  *          height: '200px',
- *          dismiss: () => showModal = false,
+ *          dismiss: () => dismissals++,
  *          content: m('', 'click border to release') 
- *      }) : undefined
+ *      })
  *    ])
  * });
  * </file>
@@ -34,15 +38,28 @@ import { m, Vnode}  from 'hslayout';
 import { ToolbarButton } from './ToolbarButton';
 
 export class Modal {
+    id = Math.floor(Math.random()*1000);
+    showModal = false;
     view(node:Vnode) {
+        const showModal = () => this.showModal;
+        const trigger = () => {
+            this.showModal = true;
+            m.redraw();
+        };
+        const dismiss = () => {
+            this.showModal = false;
+            if (node.attrs.dismiss) { node.attrs.dismiss(); }
+        };
         const w = node.attrs.width  || 'auto';
         const h = node.attrs.height || 'auto';
         const attrs = { style: `width:${w}; height:${h};`};
-        return m('.hs-modal-frame', [
-            m('.hs-modal-background', { onclick: node.attrs.dismiss}, ''),
+        if (node.attrs.setTrigger) { node.attrs.setTrigger(trigger.bind(this)); }
+        else { console.log(`required attribute function 'setTrigger' is not defined`); }
+        return !showModal()? m('span') : m('.hs-modal-frame', [
+            m('.hs-modal-background', { onclick: dismiss}, ''),
             m('.hs-modal-foreground', attrs, !node.attrs.content? 'modal pane' : [
                 node.attrs.content,
-                m(ToolbarButton, { onclick: node.attrs.dismiss, symbols:ToolbarButton.getSymbol('cross') }) 
+                m(ToolbarButton, { onclick: dismiss, symbols:ToolbarButton.getSymbol('cross') }) 
             ])
         ]);
     }
