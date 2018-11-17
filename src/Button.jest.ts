fd8f8f8facf6768515541c6147782516b6ac629d
hs <helpfulscripts@gmail.com>
2018-11-16 18:40:40 -0800

    added Button tests

--- src/Button.jest.ts
+++ src/Button.jest.ts
@@ -5,19 +5,37 @@ const mq = require('mithril-query');
 const m = require("mithril");
 
 let clicked=0;
-const out = mq(m(Button, { desc: { 
-    name: 'click me', 
-    clicked: () => clicked++ 
-}}));
 
 describe('Button', () => {
-    it ('should have DOM structure', () => {
-        out.should.have('.hs-toggle-button');
-        out.should.have('.hs-toggle-button>span');
-        out.should.have('.hs-toggle-button>span>.hs-selectable');
+    describe('with settings', () => {
+        const out = mq(m(Button, { desc: { 
+            name: 'click me', 
+            clicked: () => clicked++ 
+        }}));
+        it ('should have DOM structure', () => {
+            out.should.have('.hs-toggle-button');
+            out.should.have('.hs-toggle-button>span');
+            out.should.have('.hs-toggle-button>span>.hs-selectable');
+            out.should.contain('click me');
+        });
+        it('should click', () => {
+            out.click('.hs-selectable', null, true);
+            expect(clicked).toBe(1);
+        });
     });
-    it('should click', () => {
-        out.click('.hs-selectable', null, true);
-        expect(clicked).toBe(1);
+    describe('with defaults', () => {
+        const out = mq(m(Button, { desc: { 
+            clicked: () => clicked++ 
+        }}));
+            it ('should have DOM structure', () => {
+            out.should.have('.hs-toggle-button');
+            out.should.have('.hs-toggle-button>span');
+            out.should.have('.hs-toggle-button>span>.hs-selectable');
+            out.should.contain('button');
+        });
+        it('should click', () => {
+            out.click('.hs-selectable', null, true);
+            expect(clicked).toBe(2);
+        });
     });
 });
