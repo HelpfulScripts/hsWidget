1a230d1d304302ea1dd51ebdfb273cd6aaf54769
hs <helpfulscripts@gmail.com>
2018-11-16 18:07:04 -0800

    more Label tests

--- src/Label.jest.ts
+++ src/Label.jest.ts
@@ -1,19 +1,28 @@
 import * as hswidget from './';
 import { m }    from 'hslayout';
 
-window = Object.assign(require('mithril/test-utils/domMock.js')(), require('mithril/test-utils/pushStateMock')());
-const mq = require('mithril-query');
-
-const out = mq(m(hswidget.Label, {
-    css: '.myLabel',
-    style: 'text-align: right;',
-    text: 'This is a label'
-}));
+const root = window.document.createElement("div");
 
 describe('Label', () => {
-    it ('should have DOM structure', () => {
-        out.should.have('.hs-label');
-        out.should.have('.hs-label.myLabel');
-        out.contains('This is a label');    
+    describe('should have DOM structure', () => {
+        beforeAll(() => {
+            m.mount(root, { view: () => m(hswidget.Label, {
+                css: '.myLabel',
+                style: 'text-align: right;',
+                text: 'This is a label'
+            })});
+        });
+        it ('matches', () => {
+            expect(root).toMatchSnapshot();
+        });
+    });
+    describe('should work with defaults', () => {
+        beforeAll(() => {
+            m.mount(root, { view: () => m(hswidget.Label, {
+            })});
+        });
+        it ('matches', () => {
+            expect(root).toMatchSnapshot();
+        });
     });
 });
